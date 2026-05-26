3848c3cec9f29873cf8ea4e43b971606bb7eb687
风怳 <8523865+gerences@user.noreply.gitee.com>
2026-05-26 16:11:40 +0800

    feat(android): 初始化安卓项目构建配置与资源文件
    添加完整的安卓项目构建所需的gradle配置、启动类、资源文件以及gitignore规则，完成安卓平台项目初始化

---
+++ src-tauri/gen/android/app/src/main/java/com/mobiletavern/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mobiletavern.app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
