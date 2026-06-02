31388cea65b3a7b7f1f9d6d504f55e9d6dfe22bc
zhengjie1399677 <114562809+zhengjie1399677@users.noreply.github.com>
2026-06-02 23:02:06 +0800

    refactor: remove unused scripts and assets

--- fetch2.cjs
+++
@@ -1,4 +0,0 @@
-const https = require('https');
-https.get("https://raw.githubusercontent.com/tauri-apps/tauri/refs/heads/v2.0.0/crates/tauri-cli/templates/mobile/android/settings.gradle", (r) => {
-  let s=''; r.on('data',c=>s+=c); r.on('end', ()=>console.log(s));
-});
--- test_node.cjs
+++
@@ -1,6 +0,0 @@
-const { execSync } = require("child_process");
-try {
-  console.log(execSync(`node --print "require.resolve('@tauri-apps/cli/package.json')"`).toString());
-} catch(e) {
-  console.error(e.message);
-}
