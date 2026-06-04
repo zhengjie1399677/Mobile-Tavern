89daeb3a3439965221184b31bca7ecd025d86836
风怳 <8523865+gerences@user.noreply.gitee.com>
2026-06-04 13:06:37 +0800

    feat: initialize Tauri Android project with build configuration and CI/CD workflow

--- src-tauri/gen/android/buildSrc/build.gradle.kts
+++ src-tauri/gen/android/buildSrc/build.gradle.kts
@@ -12,6 +12,9 @@ gradlePlugin {
 }
 
 repositories {
+    maven { url = uri("https://maven.aliyun.com/repository/google") }
+    maven { url = uri("https://maven.aliyun.com/repository/public") }
+    maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
     google()
     mavenCentral()
 }
